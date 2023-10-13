126167c7b06a0d2c0b3d03d6b34e8221597b1d2a
evarg <grave432@gmail.com>
2023-10-13 16:16:29 +0200

    address form

--- src/app/sites/Addresses/addresses-new/addresses-new.component.ts
+++ src/app/sites/Addresses/addresses-new/addresses-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { countries } from 'src/app/models/countries-data';
 
 export enum AddressFCN {
   COUNTRY = 'country',
@@ -18,21 +19,37 @@ export enum AddressFCN {
   styleUrls: ['./addresses-new.component.scss'],
 })
 export class AddressesNewComponent implements OnInit {
-  public formInstance: FormGroup;
-  public FIELDS: typeof AddressFCN = AddressFCN;
+  public form: FormGroup;
+  public inputNames: typeof AddressFCN = AddressFCN;
+  public countries:any = countries
 
   constructor(private formBuilder: FormBuilder) {
-    this.formInstance = this.formBuilder.group({
-      [this.FIELDS.COUNTRY]: ['', [Validators.required, Validators.minLength(2)]],
-      [this.FIELDS.STATE]: ['', []],
-      [this.FIELDS.CITY]: ['', [Validators.required, Validators.minLength(2)]],
-      [this.FIELDS.STREET]: ['', []],
-      [this.FIELDS.HOUSE_NUMBER]: ['', []],
-      [this.FIELDS.HOME_NUMBER]: ['', []],
-      [this.FIELDS.CODE]: ['', []],
-      [this.FIELDS.STATUS]: ['', []],
+    this.form = this.formBuilder.group({
+      [this.inputNames.COUNTRY]: [
+        'Poland',
+        [Validators.required],
+      ],
+      [this.inputNames.STATE]: ['', []],
+      [this.inputNames.CITY]: [
+        '',
+        [Validators.required, Validators.minLength(2)],
+      ],
+      [this.inputNames.STREET]: ['', []],
+      [this.inputNames.HOUSE_NUMBER]: ['', []],
+      [this.inputNames.HOME_NUMBER]: ['', []],
+      [this.inputNames.CODE]: ['', []],
+      [this.inputNames.STATUS]: ['', []],
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
+  isInputValid(inputName: AddressFCN): boolean {
+    return (
+      this.form.controls[inputName].invalid &&
+      (this.form.controls[inputName].dirty ||
+        this.form.controls[inputName].touched)
+    );
+  }
 }
--- src/app/sites/Clients/clients-new/clients-new.component.ts
+++ src/app/sites/Clients/clients-new/clients-new.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+export enum ClientFCN {
+  COUNTRY = 'country',
+  STATE = 'state',
+  CITY = 'city',
+  STREET = 'street',
+  HOUSE_NUMBER = 'house_number',
+  HOME_NUMBER = 'home_number',
+  CODE = 'code',
+  STATUS = 'status',
+}
 
 @Component({
   selector: 'app-clients-new',
@@ -6,10 +18,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients-new.component.scss']
 })
 export class ClientsNewComponent implements OnInit {
+  public form: FormGroup;
+  public inputNames: typeof ClientFCN = ClientFCN;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      [this.inputNames.COUNTRY]: [
+        'Poland',
+        [Validators.required],
+      ],
+      [this.inputNames.STATE]: ['', []],
+      [this.inputNames.CITY]: [
+        '',
+        [Validators.required, Validators.minLength(2)],
+      ],
+      [this.inputNames.STREET]: ['', []],
+      [this.inputNames.HOUSE_NUMBER]: ['', []],
+      [this.inputNames.HOME_NUMBER]: ['', []],
+      [this.inputNames.CODE]: ['', []],
+      [this.inputNames.STATUS]: ['', []],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  isInputValid(inputName: ClientFCN): boolean {
+    return (
+      this.form.controls[inputName].invalid &&
+      (this.form.controls[inputName].dirty ||
+        this.form.controls[inputName].touched)
+    );
+  }
 }
